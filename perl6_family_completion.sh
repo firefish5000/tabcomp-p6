3e43b31a23bef8e80c8588f538bb013acc935911
Jhon Smith <firefish6000@gmail.com>
2014-09-09 13:42:33 -0500

    Updated Info. Added Notes.

--- perl6_family_completion.sh
+++ perl6_family_completion.sh
@@ -1,11 +1,9 @@
-# bash completion for (rakudo?) perl6
+# bash completion for the Perl6 family
 # Put this in your bash_completion directory or add
 # 'source path/to/dir/perl6_family_completion.sh' to your bashrc and
 # running shells.
 
-# Usefull looking things that may or may not exist
-# _filedir
-
+##PERL6_COMMON>>
 # Prepending with _perl6_ so there are no conflicts when sourcing multiple files
 _perl6_match() { # Succeeds if argument is repeated. Must be an EXACT MATCH.
 		#usage,  _perl6_match 'str to match' a list of str #returns fale/1; 
@@ -57,33 +55,81 @@ _perl6_seper() { # Prints the arguments that are deliminated by 'cnt' number of 
 	[ "$iterend" -eq "$cnt" ] && return 0 # may chose to do 3 way in future
 	return 1
 }
-
 # NOTE: This slows things down!
 # Taken from gentoo's adaption of Ian Macdonald's bash_completion.
 # (Adapted from bash_completion by Ian Macdonald <ian@caliban.org>)
 # This removes any options from the list of completions that have
 # already been specified on the command line.
 _perl6_rem() {	# removes arguments previously used.
-			#usage: _perl6_rem repeatable words -- associated pairs -- words we shouldn't add spaces after
-	rep_words=($(_perl6_seper '0' '--' "$@")) # It looks like we can quote all of these without error. consider doing so.
-	assoc_words=($(_perl6_seper '1' '--' "$@"))
-	nospace_words=($(_perl6_seper '2' '--' "$@"))
-		COMPREPLY=($(echo "${COMP_WORDS[@]}" | \
+				# _perl6_rem repeatable words -- associated pairs -- words we shouldn't add spaces after
+	rep_words=($(_perl6_seper '0' '--' "$@"))		# These words can be repeated 
+	assoc_words=($(_perl6_seper '1' '--' "$@"))		# These words are paired. If we have seen one, we will not sugest either.
+	nospace_words=($(_perl6_seper '2' '--' "$@"))	# these words should not have a space appended to them (eg. --output= )
+	# NOTE i wonder why he didnt use a for loop? (would IFS need to be changed?)
+	COMPREPLY=($(echo "${COMP_WORDS[@]}" | \
 		(while read -d ' ' i; do
-			[[ -z ${i} ]] && continue
-		_perl6_match "${i}" "${rep_words[@]}" && continue
-		COMPREPLY=($(_perl6_assoc "${i}" "${assoc_words[@]}"))
-		# flatten array with spaces on either side,
-			# otherwise we cannot grep on word boundaries of
-			# first and last word
-			COMPREPLY=" ${COMPREPLY[@]} "
-			# remove word from list of completions
-			COMPREPLY=("${COMPREPLY/ ${i%% *} / }") # FIXME doesnt work for --target\= and alike
+			[[ -z ${i} ]] && continue							# next word if null.
+			_perl6_match "${i}" "${rep_words[@]}" && continue		# next if word may repeat
+			COMPREPLY=($(_perl6_assoc "${i}" "${assoc_words[@]}"))	# Remove word if its pair has been typed
+			COMPREPLY=" ${COMPREPLY[@]} " # flatten array with spaces on either side, so we can grep on word boundaries of first and last word.
+			COMPREPLY=("${COMPREPLY/ ${i%% *} / }") # remove word from list of completions
 		done
 		echo ${COMPREPLY[@]})))
-	[[ "${#COMPREPLY[@]}" -eq "1" ]] && [[ "${#nospace_words[@]}" -ge '1' ]] &&  _perl6_match "$COMPREPLY" "${nospace_words[@]}" && compopt -o nospace
-		return 0
+		# If only one match was found, and that match is a nospace_word, tell bash not to add a space after the completion.
+	[[ "${#COMPREPLY[@]}" -eq '1' ]] && [[ "${#nospace_words[@]}" -ge '1' ]] && {
+		_perl6_match "$COMPREPLY" "${nospace_words[@]}" && compopt -o nospace
+	}
+	return 0
 }
+#_BC_dbg() { # used for debuging
+#	fifo_pipe="${HOME}/tmp/pipe/perl6cmdcomp.fifo"
+#	[[ -p "$fifo_pipe" ]] && echo "$@" > "$fifo_pipe"
+#}
+##PERL6_COMMON
+
+# perl6 tab completion ----
+
+
+_perl6() {
+	local cur prev words cword
+	_init_completion || return
+	longargs="--doc --help --stagestats --ll-exception --profile"
+	unlong="--target= --trace= --encoding= --output="
+	shortargs="-c -h -e -n -p -t -o -v"
+	unshort=''
+	repargs="--output --encoding -e -n -p -o" # Repeatable Arguments
+	assocargs="-h --help"	# Assosiated Arguments (only ones that cannot repeat)
+	case $prev in
+		--encoding)
+			COMPREPLY=( $( compgen -W 'utf8'  -- "$cur" ) )
+			return 0
+			;;
+	esac
+	case "$cur" in
+			'--encoding='*)
+				# FIXME our '\=' is interpoliating to '='. Fix it or make --* yeild the same, 
+				base='--encoding='
+				COMPREPLY=( $( compgen -W '${base}utf8'  -- "$cur" ) )
+				return 0
+		  		;;
+			--* )
+				COMPREPLY=( $( compgen -W '$longargs $unlong' -- "$cur" ) )
+				_perl6_rem $repargs '--' $assocargs -- $unlong
+				return 0
+				;;
+			-* )
+				COMPREPLY=( $( compgen -W '$shortargs' -- "$cur" ) )
+				_perl6_rem $repargs '--' $assocargs
+				return 0
+				;;
+			# Default, list paths
+	esac
+} # &&
+complete -F _perl6 -o default perl6{,m,j,p} perl6-debug-{m,j,p}
+
+# panda tab completion ----
+
+
 _panda_read_json_perl() { # FIXME If we cannot do it all in perl5, we should do it all in perl6.
 perl <<'HEREPERL'
 	use feature qw{say};
@@ -123,10 +169,11 @@ HEREPERL
 }
 
 _panda_modules() { # TIMTOWTDI. (actualy, I think the container may change in future versions(or implimintations).. but feel free to try seding/greping/whatever instead)
-	out="$(_panda_read_json_perl)" # NOTE I think $out should hold fine, but depending on what chars are allowed for perl6mods, we may run into trubble
+	# store the output into $out. so we can check if modules were found and try another command if it failed.
+	out="$(_panda_read_json_perl 2>/dev/null)" # First try to read any/all projects.json files (created by panda on update/list/search/install)
 	[[ -z "$out" ]] && { 
 		# Last resort, use 'panda list'. This both alows us to do completion without a projects.json,
-		out="$(panda list | awk '{print $1}')" # and generates one for faster completion on next run
+		out="$(panda list | awk '{print $1}')" 2>/dev/null # and generates one for faster completion next time
 	}
 	echo "$out"
 	unset out
@@ -167,46 +214,62 @@ _panda() {
 		COMPREPLY=( $( compgen -W '$list_args $install_args update info search install list' -- "$cur" ) )
 	fi
 }
-#_BC_dbg() { # used for debuging
-#	fifo_pipe="${HOME}/tmp/pipe/perl6cmdcomp.fifo"
-#	[[ -p "$fifo_pipe" ]] && echo "$@" > "$fifo_pipe"
-#}
-_rakudobrew_inst_versions() { # finds all ackends and versions installed. (in backend-version notation)
-perl <<"HERE_INST_VERSIONS"
-	use feature q{say};
-	opendir my $dh, join(q{/}, $ENV{HOME}, q{.rakudobrew});
-	while (readdir($dh)) {
-		next unless ($_ =~ /^moar/);
-		say $_;
-	}
-	closedir($dh)
-HERE_INST_VERSIONS
-}
-_rakudobrew() { # Tab completion for rakudo brew.
-	local cur prev words cword
-	_init_completion || return
+
+complete -F _panda -o default panda
+
+# rakudobrew tab completion ----
+
+
+_rakudobrew_dir() {
+	# in case some form of config file appears? 
+	# I added this under the *mostly* incorrect assumption that rakudobrew used the installation dir for storing files
+	# (which would be a bad idea since it wouldn't work for system installations)
 	brewdir="${HOME}/.rakudobrew"
-	backends="parrot jvm moar moar_jit"
-	versions="2014.08 2014.07 2014.06 2014.05 2014.04 2014.03 2014.02 2014.01 HEAD"
+	echo "$brewdir";
+}
+_rakudobrew_inst_versions() { # finds all backends and versions installed. (in backend-version notation)
+	brewdir="$(_rakudobrew_dir)"
+	{ find "$brewdir" -mindepth 1 -maxdepth 1 -type d -regextype posix-extended -regex "${brewdir}"'/(moar|moar_jit|parrot|jvm)\-(HEAD|.*)' | sed 's/.*\///'; } 2>/dev/null
+}
+
+_rakudobrew_impl() {
+	 brewdir="$(_rakudobrew_dir)";
+	 baserepo="$(_rakudobrew_inst_versions | head -n 1)"
+	 pushd "${brewdir}/${baserepo}" 2>&1>/dev/null;
+	 git tag -l 2>/dev/null;
+}   
+
+_rakudobrew() { # Tab completion for rakudo brew.
+	local cur prev words cword;
+	_init_completion || return;
+	brewdir="$(_rakudobrew_dir)";
+	backends="parrot jvm moar moar_jit";
+	versions="HEAD";
 	case $prev in
-			switch)
-				COMPREPLY=( $( compgen -W '$( _rakudobrew_inst_versions )'  -- "$cur" ) )
-				_perl6_rem
-				return 0
-		  		;;
-			build)
-				COMPREPLY=( $( compgen -W 'all $backends'  -- "$cur" ) )
-				_perl6_rem 
-				return 0
-		  		;;
+			 switch)
+				 COMPREPLY=( $( compgen -W '$( _rakudobrew_inst_versions )'  -- "$cur" ) );
+				 _perl6_rem;
+				 return 0;
+				 ;;
+			 build)
+				 COMPREPLY=( $( compgen -W 'all $backends'  -- "$cur" ) );
+				 _perl6_rem;
+				 return 0;
+				 ;;
 	esac
 	if  ( [[ "${#COMP_WORDS[@]}" -ge '3' ]] && [[ "${COMP_WORDS[-3]}" == "build" ]] && _perl6_match "$prev" $backends ); then
-		COMPREPLY=( $( compgen -W '$versions'  -- "$cur" ) ) # TODO version
+		 COMPREPLY=( $( compgen -W "$versions $(_rakudobrew_impl)"  -- "$cur" ) );
 	elif [[ "$cur" == * ]]; then
-		COMPREPLY=( $( compgen -W 'switch rehash list current build build-panda' -- "$cur" ) )
-		_perl6_rem 
-	fi
+		 COMPREPLY=( $( compgen -W 'switch rehash list current build build-panda' -- "$cur" ) );
+		 _perl6_rem;
+	fi;
 }
+
+complete -F _rakudobrew -o default rakudobrew
+
+# padre tab completion ----
+
+
 _padre() {
 	local cur prev words cword
 	_init_completion || return
@@ -221,42 +284,4 @@ _padre() {
 	esac
 	
 }
-_perl6() {
-	local cur prev words cword
-	_init_completion || return
-	longargs="--doc --help --stagestats --ll-exception --profile"
-	unlong="--target= --trace= --encoding= --output="
-	shortargs="-c -h -e -n -p -t -o -v"
-	unshort=''
-	repargs="--output --encoding -e -n -p -o" # Repeatable Arguments
-	assocargs="-h --help"	# Assosiated Arguments (only ones that cannot repeat)
-	case $prev in
-		--encoding)
-			COMPREPLY=( $( compgen -W 'utf8'  -- "$cur" ) )
-			return 0
-			;;
-	esac
-	case "$cur" in
-			'--encoding='*)
-				# FIXME our '\=' is interpoliating to '='. Fix it or make --* yeild the same, 
-				base='--encoding='
-				COMPREPLY=( $( compgen -W '${base}utf8'  -- "$cur" ) )
-				return 0
-		  		;;
-			--* )
-				COMPREPLY=( $( compgen -W '$longargs $unlong' -- "$cur" ) )
-				_perl6_rem $repargs '--' $assocargs -- $unlong
-				return 0
-				;;
-			-* )
-				COMPREPLY=( $( compgen -W '$shortargs' -- "$cur" ) )
-				_perl6_rem $repargs '--' $assocargs
-				return 0
-				;;
-			# Default, list paths
-	esac
-} # &&
-complete -F _perl6 -o default perl6{,m,j,p} perl6-debug-{m,j,p}
-complete -F _panda -o default panda
-complete -F _rakudobrew -o default rakudobrew
 complete -F _padre -o default padre
